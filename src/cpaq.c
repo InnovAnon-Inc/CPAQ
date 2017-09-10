8f8cc45bc834e911f0639d5d354f3e8a966c5d6a
Default User <laurence.a.maddox@gmail.com>
2017-09-10 18:16:51 -0500

    adding warnings

--- src/cpaq.c
+++ src/cpaq.c
@@ -27,7 +27,7 @@ __attribute__ ((nonnull (1), nothrow, warn_unused_result))
 int alloc_queue (
    cpaq_t *restrict q,
    size_t n) {
-   void *arr = malloc (n * sizeof (void *));
+   void *restrict arr = malloc (n * sizeof (void *));
    error_check (arr == NULL) return -1;
    init_queue (q, arr, n);
    return 0;
@@ -43,7 +43,7 @@ int enqueue (
    cpaq_t *restrict q,
    void const *restrict elem) {
 	/*puts ("enqueue ()");*/
-   if (isfull (q)) return -1;
+   error_check (isfull (q) != false) return -1;
    q->Q[q->tail] = elem;
    q->tail = (q->tail + 1) % q->n;
    return 0;
@@ -51,8 +51,8 @@ int enqueue (
 
 __attribute__ ((nonnull (1), nothrow, warn_unused_result))
 void const *dequeue (cpaq_t *restrict q) {
-   void *x;
-   if (isempty (q)) return NULL;
+   void const *restrict x;
+   error_check (isempty (q) != false) return NULL;
    x = q->Q[q->head];
    q->head = (q->head + 1) % q->n;
    return x;
@@ -70,7 +70,7 @@ bool isfull (cpaq_t const *restrict q) {
 
 __attribute__ ((nonnull (1), nothrow, pure, warn_unused_result))
 void const *gethead(cpaq_t const *restrict q) {
-   if (isempty (q)) return NULL;
+   error_check (isempty (q) != false) return NULL;
    return q->Q[q->head];
 }
 
@@ -78,7 +78,7 @@ __attribute__ ((nonnull (1), nothrow))
 void dumpq(
    cpaq_t const *restrict q,
    int i) {
-   void *head = gethead (q);
+   void const *restrict head = gethead (q);
    printf("i:%i, head:%i,", i, (int) q->head);
    if (head == NULL)
       printf(" tail:%i, Q[head]:NULL\n",
