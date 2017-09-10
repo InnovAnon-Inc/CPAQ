ceba7b64d2874fed46a819ed237ab85abbe3d22b
Default User <laurence.a.maddox@gmail.com>
2017-09-10 14:05:27 -0500

    fixing warnings

--- src/cpaq-test.c
+++ src/cpaq-test.c
@@ -11,19 +11,15 @@ int main(void) {
    int *tmp;
 
    cpaq_t q;
-   alloc_queue (&q, sizeof (int), (size_t) N);
+   alloc_queue (&q, (size_t) N);
 
    dumpq(&q, 1);
-   *(int*)enqueue(&q) = arr[0];dumpq(&q, 2);
-   *(int*)enqueue(&q) = arr[1];dumpq(&q, 3);
-   *(int*)enqueue(&q) = arr[2];dumpq(&q, 4);
-   tmp = enqueue(&q);
-   if (tmp != NULL) *tmp = arr[3];
-   dumpq(&q, 5);
-   tmp = enqueue(&q);
-   if (tmp != NULL) *tmp= arr[4];
-   dumpq(&q, 6);
-   memset (arr, 0, sizeof (arr));
+   enqueue (&q, arr + 0);dumpq(&q, 2);
+   enqueue (&q, arr + 1);dumpq(&q, 3);
+   enqueue (&q, arr + 2);dumpq(&q, 4);
+   enqueue (&q, arr + 3);dumpq(&q, 5);
+   enqueue (&q, arr + 4);dumpq(&q, 6);
+   /*memset (arr, 0, sizeof (arr));*/
    printf("%i\n", *(int *) dequeue(&q));
       dumpq(&q, 7);
    printf("%i\n", *(int *) dequeue(&q));
